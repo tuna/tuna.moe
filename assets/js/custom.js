41af85b3d6f95e996250df81474866c45264ae03
Harry Chen <i@harrychen.xyz>
2017-12-30 21:36:21 +0800

    Add 征婚 for pero
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,5 +8,63 @@
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 	}); 
 
-		
+        jQuery(document).ready(function() {
+
+          var speed = 2;
+          var omega = 1;
+
+          var dx = speed;
+          var dy = speed;
+
+          var paused = false;
+
+          var $pero = $('#pero');
+          $pero.mouseenter(function() {
+            paused = true;
+            $('.zhenghun').addClass('superzhenghun');
+          });
+          $pero.mouseout(function() {
+            paused = false;
+            $('.zhenghun').removeClass('superzhenghun');
+          });
+
+          var curX = 0;
+          var curY = 0;
+          
+          var rotation = 0;
+          function frame() {
+            var $pero = $('#pero');
+
+            if(!paused) {
+              curX += dx;
+              curY += dy;
+
+              if(curY + $pero.height() >= window.innerHeight) {
+                dy = -speed;
+                omega = -omega;
+              }
+              if(curY <= 0) {
+                dy = speed;
+                omega = -omega;
+              }
+
+              if(curX + $pero.width() >= window.innerWidth) {
+                dx = -speed;
+                omega = -omega;
+              }
+              if(curX <= 0) {
+                dx = speed;
+                omega = -omega;
+              }
+            }
+
+            rotation += omega;
+            
+            $pero.css('transform', 'translate(' + curX + 'px, ' + curY + 'px) rotate(' + rotation + 'deg)');
+
+            requestAnimationFrame(frame);
+          }
+
+          frame();
+        });
 })(jQuery);
